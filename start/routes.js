f17b0d7020c789f497fec05e1d037498d1398ef1
welldheeraj <welldheeraj@hotmail.com>
2025-05-03 17:02:40 +0530

    v1

---
+++ start/routes.js
@@ -0,0 +1,15 @@
+
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+
+Route.on('/').render('welcome');
+
+Route.get('users', async (req, res) => {
+    //const users = await User.all();
+    //res.json('hhh');
+    return "jjj";
+});
+Route.any('users1', async (req, res) => {
+    return "jjj";
+});
